e47fe2d8bcf69c405ce65af1ca183c2173557759
Simona Hamborová <simona.hamborova@student.tuke.sk>
2022-11-13 12:15:14 +0100

    rano

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -5,7 +5,7 @@ import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 import sk.tuke.kpi.oop.game.actions.PerpetualReactorHeating;
-import sk.tuke.kpi.oop.game.tools.FireExtinguisher;
+
 
 import java.util.HashSet;
 import java.util.Set;
