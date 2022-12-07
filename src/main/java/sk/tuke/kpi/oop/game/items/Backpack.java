4eb494c726a0e424f371433951f2884ceb1433ce
Simona Hamborová <simona.hamborova@student.tuke.sk>
2022-12-07 22:00:42 +0100

    backpack iterator

--- src/main/java/sk/tuke/kpi/oop/game/items/Backpack.java
+++ src/main/java/sk/tuke/kpi/oop/game/items/Backpack.java
@@ -9,21 +9,21 @@ import java.util.List;
 public class Backpack implements ActorContainer<Collectible> {
     private String name;
     private List<Collectible> items = new ArrayList<>();
-    private Backpack backpack;
+
     private int capacity;
-    private int count_items;
+
     private Collectible one_item;
     public Backpack(String name, int capacity){
             this.name = name;
             this.capacity = capacity;
-            this.count_items = 0;
-    }
 
+    }
+    @Override
     public int getCapacity() {
         return capacity;
     }
-
-    public String getName() {
+    @Override
+    public @NotNull String getName() {
         return name;
     }
 
@@ -34,7 +34,7 @@ public class Backpack implements ActorContainer<Collectible> {
 
     @Override
     public int getSize() {
-        return count_items;
+        return items.size();
     }
 
     @Override
@@ -47,7 +47,7 @@ public class Backpack implements ActorContainer<Collectible> {
 
     @Override
     public void remove(@NotNull Collectible actor) {
-        if(items.size()<=0){
+        if(items.size()==0){
             throw new IllegalStateException();
         }
         items.remove(actor);
@@ -56,15 +56,14 @@ public class Backpack implements ActorContainer<Collectible> {
     @NotNull
     @Override
     public Iterator<Collectible> iterator() {
-        for (Collectible item : backpack) {
-
-            // pouzitie predmetu (item) z batohu
-        }
-        return null;
+        return items.iterator();
     }
 
     @Override
     public @Nullable Collectible peek() {
+        if(items.size()<1){
+            return items.get(items.size()-1);
+        }
         //System.out.println(items.get(items.size()-1));
         return items.get(items.size()-1);
     }
@@ -75,7 +74,7 @@ public class Backpack implements ActorContainer<Collectible> {
             return;
         }
         one_item=items.get(items.size()-1);
-        int from_zero =0;
+
         items.add(0,one_item);
         //System.out.println(items.size());
         items.remove(items.size()-1);
