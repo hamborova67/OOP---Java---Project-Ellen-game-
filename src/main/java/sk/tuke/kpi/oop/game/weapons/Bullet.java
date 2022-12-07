feb0c2f0d3562b25008c765226edba8aced5193a
Simona Hamborová <simona.hamborova@student.tuke.sk>
2022-12-07 23:24:47 +0100

    nahodne veci opravene, idem do sprchy:)

--- src/main/java/sk/tuke/kpi/oop/game/weapons/Bullet.java
+++ src/main/java/sk/tuke/kpi/oop/game/weapons/Bullet.java
@@ -1,21 +1,28 @@
 package sk.tuke.kpi.oop.game.weapons;
 
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.graphics.Animation;
 
 public class Bullet extends AbstractActor implements Fireable {
+    private int speed;
+    private int ammo;
+
     public Bullet(){
+        Animation bullet =  new Animation("sprites/bullet.png",16,16);
+        speed=1;
 
     }
 
 
     @Override
     public int getSpeed() {
-        return 0;
+        return speed;
     }
+
 
     @Override
     public int getAmmo() {
-        return 0;
+        return ammo;
     }
 
     @Override
