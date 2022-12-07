9384735b3da92a48d9fd3700a5a8a128c0e61334
Simona Hamborová <simona.hamborova@student.tuke.sk>
2022-12-07 22:24:46 +0100

    drop

--- src/main/java/sk/tuke/kpi/oop/game/actions/Drop.java
+++ src/main/java/sk/tuke/kpi/oop/game/actions/Drop.java
@@ -1,13 +1,25 @@
 package sk.tuke.kpi.oop.game.actions;
 import sk.tuke.kpi.gamelib.framework.actions.AbstractAction;
 import sk.tuke.kpi.oop.game.Keeper;
+import sk.tuke.kpi.oop.game.items.Collectible;
 
 public class Drop<K extends Keeper> extends AbstractAction<K> {
+    private Collectible collectible;
     public Drop(){
 
     }
     @Override
     public void execute(float deltaTime) {
+        if(getActor().getBackpack().getSize() < 1 || getActor() ==null || getActor().getScene()==null){
+            setDone(true);
+            return;
+        }
+        if(!isDone()){
+            collectible = getActor().getBackpack().peek();
+            getActor().getScene().addActor(collectible, getActor().getPosX(), getActor().getPosY());
+            getActor().getBackpack().remove(collectible);
+            setDone(true);
+        }
 
     }
 }
