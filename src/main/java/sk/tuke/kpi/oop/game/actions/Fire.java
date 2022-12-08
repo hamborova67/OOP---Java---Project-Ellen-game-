2254385adff5f30c8055edb8e0180083771120bd
Simona Hamborová <simona.hamborova@student.tuke.sk>
2022-12-08 16:02:18 +0100

    fireworks

--- src/main/java/sk/tuke/kpi/oop/game/actions/Fire.java
+++ src/main/java/sk/tuke/kpi/oop/game/actions/Fire.java
@@ -1,7 +1,9 @@
 package sk.tuke.kpi.oop.game.actions;
-
+import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.framework.actions.AbstractAction;
+import sk.tuke.kpi.oop.game.Direction;
 import sk.tuke.kpi.oop.game.characters.Armed;
+import sk.tuke.kpi.oop.game.weapons.Fireable;
 
 public class Fire<A extends Armed> extends AbstractAction<A> {
     public Fire(){
@@ -10,6 +12,37 @@ public class Fire<A extends Armed> extends AbstractAction<A> {
 
     @Override
     public void execute(float deltaTime) {
+        if(getActor() ==null || isDone()){
+            setDone(true);
+            return;
+        }
+
+        Fireable fireable = getActor().getFirearm().fire();
+
+        if(fireable==null){
+            setDone(true);
+            return;
+        }
+
+        int x = Direction.fromAngle(getActor().getAnimation().getRotation()).getDx();
+        int y = Direction.fromAngle(getActor().getAnimation().getRotation()).getDy();
+        Scene scene = getActor().getScene();
+        Direction smergulky = Direction.fromAngle(getActor().getAnimation().getRotation());
+        if(scene==null || smergulky==null){
+            setDone(true);
+            return;
+        }
+
+        scene.addActor(fireable,getActor().getPosX()+x,getActor().getPosY()+y);
+        fireable.startedMoving(smergulky);
+        new Move<Fireable>(smergulky,Float.MAX_VALUE).scheduleFor(fireable);
+
+
+
+
+
+
+
 
     }
 }
