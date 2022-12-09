af8d2efb524468d0edae8b30b46b44645a390b37
Simona Hamborová <simona.hamborova@student.tuke.sk>
2022-12-09 02:03:50 +0100

    alien oprava?

--- src/main/java/sk/tuke/kpi/oop/game/characters/Alien.java
+++ src/main/java/sk/tuke/kpi/oop/game/characters/Alien.java
@@ -1,7 +1,12 @@
 package sk.tuke.kpi.oop.game.characters;
 import org.jetbrains.annotations.NotNull;
+import sk.tuke.kpi.gamelib.Actor;
 import sk.tuke.kpi.gamelib.Scene;
+import sk.tuke.kpi.gamelib.actions.ActionSequence;
+import sk.tuke.kpi.gamelib.actions.Invoke;
+import sk.tuke.kpi.gamelib.actions.Wait;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 import sk.tuke.kpi.oop.game.Movable;
 import sk.tuke.kpi.oop.game.behaviours.Behaviour;
@@ -52,7 +57,31 @@ public class Alien extends AbstractActor implements Alive, Enemy, Movable {
             return;
         }
         behaviour.setUp(this);
+        drain();
+
     }
 
+    public void drain(){
+        if(getScene()==null){
+            return;
+        }
+        List<Actor> list;
+        list = getScene().getActors();
+
+        for (Actor alive : list) {
+            if (alive instanceof Alive && this.intersects(alive) && !(alive instanceof Enemy)) {
+                ((Alive) alive).getHealth().drain(3);
+                new Loop<>(
+                new ActionSequence<>(
+                    new Invoke<>(this::drain),
+                    new Wait<>(2)
+                )).scheduleFor(this);
+
+            }
+        }
+    }
 
 }
+
+
+
